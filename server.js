e503005f685e52fa9e37898cba4a86a458c7ae02
Evan Griffiths <griffithse@gmail.com>
2015-04-18 12:46:06 -0400

    more testing

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true, limit: 26214400}));
 
 app.post("/inbound", function(req,res) {
-  var event = JSON.parse(req.body.mandrill_events);
+  console.log("1111111111111");
+  console.log(req.body.mandrill_events);
+  console.log("2222222222222");
+  console.log(req.body.mandrill_events[0]);
+  var event = JSON.parse(req.body.mandrill_events[0]);
+  console.log("3333333333333");
+  console.log(event);
   var summary = {
     text: event.text,
     fromemail: event.from_email,
