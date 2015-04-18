c250480bd5c39d33f52668be4b121ad1c53f2298
Evan Griffiths <griffithse@gmail.com>
2015-04-18 12:42:23 -0400

    some parsing done

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({extended: true, limit: 26214400}));
 app.post("/inbound", function(req,res) {
   var event = JSON.parse(req.body.mandrill_events);
   var summary = {
-    text: event.text
-    fromemail: event.from_email
-    fromname: event.from_name
+    text: event.text,
+    fromemail: event.from_email,
+    fromname: event.from_name,
     subject: event.subject
   };
   console.log(summary);
