584be29a006e8ac817d8fab222d4ed4577cb9dba
Evan Griffiths <griffithse@gmail.com>
2015-04-18 12:58:12 -0400

    more testing

--- server.js
+++ server.js
@@ -6,29 +6,34 @@ app.use(bodyParser.urlencoded({extended: true, limit: 26214400}));
 
 app.post("/inbound", function(req,res) {
   try {
-    console.log("1111111111111");
-    console.log(req.body.mandrill_events);
-    console.log("2222222222222");
+    var msgarray = JSON.parse(req.body.mandrill_events);
+    var msg0 = msgarray[0];
     try {
-      console.log(JSON.parse(req.body.mandrill_events));
+      var summary = {
+        text: msg0.text,
+        fromemail: msg0.from_email,
+        fromname: msg0.from_name,
+        subject: msg0.subject
+      };
+      console.log(summary);
     } catch (e) {
       console.log("parseerror 1: "+e);
     }
-    console.log(req.body.mandrill_events[0]);
-    try {
-      var event = JSON.parse(req.body.mandrill_events[0]);
-    } catch (err) {
-      console.log("parse error: "+err);
-    }
-    console.log("3333333333333");
-    console.log(event);
-    var summary = {
-      text: event.text,
-      fromemail: event.from_email,
-      fromname: event.from_name,
-      subject: event.subject
-    };
-    console.log(summary);
+    // console.log(req.body.mandrill_events[0]);
+    // try {
+    //   var event = JSON.parse(req.body.mandrill_events[0]);
+    // } catch (err) {
+    //   console.log("parse error: "+err);
+    // }
+    // console.log("3333333333333");
+    // console.log(event);
+    // var summary = {
+    //   text: event.text,
+    //   fromemail: event.from_email,
+    //   fromname: event.from_name,
+    //   subject: event.subject
+    // };
+    // console.log(summary);
   } catch (e) {
     console.log("error: "+e);
   }
