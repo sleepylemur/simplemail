218c4fabf1e7c86e944c7846ce2fec26e38fa522
Evan Griffiths <griffithse@gmail.com>
2015-04-18 13:46:03 -0400

    sendmail working

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var secrets = require('./secrets.json');
+
+var transporter = nodemailer.createTransport(smtpTransport({
+  host: "smtp.mandrillapp.com",
+  port: 587,
+  auth: secrets.mandrill
+}));
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: true, limit: 26214400}));
+
+// app.get("/testemail", function(req,res) {
+//   console.log(secrets.mandrill);
+//     try {
+//       var mailOptions = {
+//         from: 'evan <evan@evangriffiths.nyc>',
+//         to: 'griffithse@gmail.com',
+//         subject: 'hi me',
+//         text: 'check this shit: '
+//       };
+//       transporter.sendMail(mailOptions, function(err,info) {
+//         if (err) {
+//           console.log("sendMail error: "+err);
+//         } else {
+//           console.log("sent: "+info.response);
+//         }
+//       });
+//     } catch (e) {
+//       console.log(e);
+//     }
+//     res.end();
+// });
 
 app.post("/inbound", function(req,res) {
   try {
@@ -22,6 +53,24 @@ app.post("/inbound", function(req,res) {
       console.log(summary);
     } catch (e) {
       console.log("parseerror 1: "+e);
+      summary = {error: e, message: "failed to parse"};
+    }
+    try {
+      var mailOptions = {
+        from: 'evan <evan@evangriffiths.nyc>',
+        to: 'griffithse@gmail.com',
+        subject: 'hi me',
+        text: 'check this shit: '+JSON.stringify(summary)
+      };
+      transporter.sendMail(mailOptions, function(err,info) {
+        if (error) {
+          console.log("sendMail error: "+error);
+        } else {
+          console.log("sent: "+info.response);
+        }
+      });
+    } catch (e) {
+      console.log(e);
     }
   } catch (e) {
     console.log("error: "+e);
@@ -29,4 +78,4 @@ app.post("/inbound", function(req,res) {
   res.end();
 });
 
-app.listen(4000, function() {console.log("listening emails on port 4000");});
+app.listen(4000, function() {console.log("listening for emails on port 4000");});
